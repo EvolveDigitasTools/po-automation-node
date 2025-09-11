7a6c4f7599ab4205319b15ee8b728caf8c45b5ff
Evolve <jatina@evolvedigitas.com>
2025-09-11 16:52:36 +0530

    Moved backend files into backend/ folder and added frontend/ folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import './index.css'
+import SKUManager from './components/SKUManager'
+
+function App() {
+  return (
+    <>
+      <SKUManager />
+    </>
+  )
+}
+
+export default App
